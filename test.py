dc6448fff8bc68afa3221a29c729b0729c9bb3e4
amanpal2412 <43093104+amanpal2412@users.noreply.github.com>
2023-01-31 16:49:19 +0530

    created python function

---
+++ test.py
@@ -0,0 +1,52 @@
+try:
+    import json
+    import boto3
+    from selenium.webdriver import Chrome
+    from selenium.webdriver.chrome.options import Options
+    import os
+    import shutil
+    import uuid
+    import time
+    from datetime import datetime
+    import datetime
+
+    print("All Modules are ok ...")
+    
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    s3 = boto3.client('s3')
+
+except Exception as e:
+
+    print("Error in Imports ")
+
+
+
+class WebDriver(object):
+
+    def __init__(self):
+        self.options = Options()
+
+        self.options.binary_location = '/opt/headless-chromium'
+        self.options.add_argument('--headless')
+        self.options.add_argument('--no-sandbox')
+        self.options.add_argument('--start-maximized')
+        self.options.add_argument('--start-fullscreen')
+        self.options.add_argument('--single-process')
+        self.options.add_argument('--disable-dev-shm-usage')
+
+    def get(self):
+        driver = Chrome('/opt/chromedriver', options=self.options)
+        return driver
+
+
+
+def lambda_handler(event, context):
+
+    instance_ = WebDriver()
+    driver = instance_.get()
+    driver.get("https://www.youtube.com")
+    time.sleep(5)
+    driver.save_screenshot("/tmp/image.png")
+    s3.upload_file('/tmp/image.png', 'testseleniumchromedriver', 'image.png_'+timestr)
+    print(driver.page_source)
+    return True
