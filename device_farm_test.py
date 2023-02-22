8eac45327ff2c7fb90f59431415502721ef5e6ad
amanpal2412 <43093104+amanpal2412@users.noreply.github.com>
2023-02-22 13:50:26 +0530

    Update device_farm_test.py

--- device_farm_test.py
+++ device_farm_test.py
@@ -15,8 +15,14 @@ remote_url = devicefarm.create_test_grid_url(
     expiresInSeconds=300
 )
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--window-size=1920,1080")
+
 driver = webdriver.Remote(remote_url["url"],
-                          webdriver.DesiredCapabilities.CHROME)
+                          options=chrome_options)
 
 try:
     driver.get("https://www.youtube.com")
