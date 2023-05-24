a0048df0c764a78c9a5a75e6553f38c7634af0cc
amanpal2412 <43093104+amanpal2412@users.noreply.github.com>
2023-05-24 19:37:12 +0530

    Update device_farm_test.py

--- device_farm_test.py
+++ device_farm_test.py
@@ -12,15 +12,14 @@ devicefarm = boto3.client('devicefarm', region_name='us-west-2')
 
 remote_url = devicefarm.create_test_grid_url(
     projectArn="arn:aws:devicefarm:us-west-2:482218046771:testgrid-project:0d0a9d1b-c4b0-4393-a4c1-e07e25686ceb",
-    expiresInSeconds=300
+    expiresInSeconds=900
 )
 
-driver = webdriver.Remote(remote_url["url"],
-                          webdriver.DesiredCapabilities.CHROME)
+driver = webdriver.Remote(remote_url["url"], webdriver.DesiredCapabilities.CHROME)
 
 try:
     driver.get("https://www.youtube.com")
-    time.sleep(5)
+    time.sleep(120)
     driver.save_screenshot("/tmp/image.png")
     object_key = 'image-' + timestr + '.png'
     s3.upload_file('/tmp/image.png', 'testseleniumchromedriver', object_key)
